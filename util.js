35291f6af81f0556ea847a69e5e1e071fdfadee1
rlawndks4 <rlawndks0423@naver.com>
2023-04-19 12:04:56 +0900

    asdf

--- util.js
+++ util.js
@@ -486,7 +486,7 @@ const initialPay = async (contract) => { // 월세 내역 추가
                 pay_date[1] = `${pay_date[1] >= 10 ? pay_date[1] : `0${pay_date[1]}`}`
                 pay_date = `${pay_date[0]}-${(pay_date[1])}-${contract_day}`
             }
-            if (pay_date <= now && pay_date >= contract.start_date || now.substring(0, 7) == pay_date.substring(0, 7)) {
+            if ((pay_date <= now && pay_date >= contract.start_date) || now.substring(0, 7) == pay_date.substring(0, 7)) {
                 pay_list.push(
                     [
                         contract[`${getEnLevelByNum(0)}_pk`],
